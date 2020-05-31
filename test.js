b459fa460d5a23501e2abcdbada34274f9a01561
Junhao Zhang <thepuzzlecoderuser@gmail.com>
2020-05-31 12:31:28 -0500

    Fixed a few bugs

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 const Replit = require("./index");
 const fs = require("fs");
-const client = new Replit();
+const bot = new Replit();
+// {"m":[]}
 (async () => {
   let data = JSON.parse(fs.readFileSync("private", "utf8"));
-  await client.login(data.username, data.password)
-  client.listen("index.json");
+  await bot.login(data.username, data.password)
+  bot.checkPosts([39768]);
+  bot.checkFrequency(1000);
+  bot.on("message", async data => {
+    let output = await data.actions.reply(data.message.id, "Hello world! Your message was "+data.message.body);
+    console.log(data.actions);
+    console.log(data.message.id, "Hello world! Your message was "+data.message.body);
+    console.log(output);
+  })
+  bot.listen("index.json");
 })()
